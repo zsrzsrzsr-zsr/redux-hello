1dba9bd081db3d2dfeb74b42950130665bbeea0b
zsr <691363707@qq.com>
2018-01-23 12:24:24 +0800

    Updates

--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from 'redux'
-import rootReducer from '../reducers'
-import logger from 'redux-logger'
-import thunk from 'redux-thunk'
-
-const store = createStore(rootReducer, applyMiddleware(logger, thunk))
-
-export default store
